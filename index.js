0bb8d2e8beaa3bc1f3e34a656296140eb26a72fa
sonujavascript <142430182+sonujavascript@users.noreply.github.com>
2024-03-29 08:17:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function blue(){
+    btn.style.color = 'white';
+    btn.style.backgroundColor = 'orange';
+    btn.style.fontSize = '1.55rem' ;
+    btn.style.padding = '6px 20px' ;
+}
+
+function black(){
+    btn.style.backgroundColor = 'brown';
+    btn.style.color = 'white';
+    btn.style.fontSize = '1.5rem' ;
+    btn.style.padding = '6px 20px' ;
+}
+
+function mouseOver(element){
+    element.style.fontSize = '1.4rem';
+}
+
+function mouseOut(element){
+    element.style.fontSize = '1.3rem'
+}
